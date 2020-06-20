0bd2a6c79c4485d00ffee1c6ed5a158091f09338
onesword0618 <ao.akua.leo@gmail.com>
2020-06-20 16:57:16 +0900

    Fix: spotlessによる異常の修正

--- src/test/java/jp/gushed/mate/infrastructure/internal/logging/LoggingFormatterTest.java
+++ src/test/java/jp/gushed/mate/infrastructure/internal/logging/LoggingFormatterTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.when;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
